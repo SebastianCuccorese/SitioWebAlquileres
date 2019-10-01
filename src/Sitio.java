d2623ac5555a65d5a99bfe42a2137781dc42fd9d
emiliana <emiliana.ailen@hotmail.com>
2019-10-01 17:15:22 -0300

    modificaciones en busqueda de propiedades

--- src/Sitio.java
+++ src/Sitio.java
@@ -1,4 +1,4 @@
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -6,40 +6,79 @@ import java.util.stream.Collectors;
 public class Sitio {
     private List<Propietario> listaPropietarios = new ArrayList<>();
     private List<Inquilino>  listaInquilinos = new ArrayList<>();
+    private List<Reserva> listaDeReservas = new ArrayList<>();
+
+    private boolean hayDisponibilidad(Propiedad propiedad, LocalDate fechaDeIngreso, LocalDate fechaDeSalida){
+        return !this.existeReservaDePropiedad(propiedad) || this.getListaDeReservas().stream().anyMatch(reserva -> reserva.getPropiedad() == propiedad &&(
+                reserva.getFechaDeIngreso().isAfter(fechaDeSalida) || reserva.getFechaDeSalida().isBefore(fechaDeIngreso)));
+    }
+
+    private boolean existeReservaDePropiedad(Propiedad unaPropiedad) {
+        return this.getListaDeReservas().stream().anyMatch(r-> r.getPropiedad() == unaPropiedad);
+    }
+
+    //valores default para parametros de busqueda no obligatorios
+    private Integer defaultcantHuespedes(Integer cant){
+        if (cant != null){
+            return cant;
+        }
+        else return 1;
+    }
+
+    private Integer defaultPrecioMin(Integer precioMin){
+        if (precioMin != null){
+            return precioMin;
+        }
+        else return 0;
+    }
+
+    private Integer defaultPrecioMax(Integer precioMax){
+        if (precioMax != null){
+            return precioMax;
+        }
+        else return 2000000000;  //buscar si hay definicion de infinito, esto es medio caca
+    }
+
+    /*Bueno con esto creo que no hay problemas de repetidos, no me convence lo de los valores default para los parametros opcionales.
+    Agregue una lista de reservas en el sitio para conocer la disponibilidad de una propiedad para la busqueda, despues hay que ver donde
+    quedaria mejor esa lista.*/
+
+    public List<Propiedad> buscarPropiedad(String ciudad, LocalDate fechaIngreso, LocalDate fechaSalida ,Integer cantHuespedes, Integer  precioMin,Integer  precioMax) {
+        List<Propiedad>  listaPropiedadesAdecuadas = new ArrayList<>();
+
+        for(Propietario propietario : this.getListaPropietarios()) {
+            listaPropiedadesAdecuadas.addAll((propietario.getListaPropiedades().stream().filter(propiedad -> propiedad.getCiudad().equals(ciudad) &&
+                    hayDisponibilidad(propiedad, fechaIngreso, fechaSalida) && propiedad.getCapacidad() >= this.defaultcantHuespedes(cantHuespedes) &&
+                    propiedad.getPrecio()>= this.defaultPrecioMin(precioMin) && propiedad.getPrecio()<= this.defaultPrecioMax(precioMax))).collect(Collectors.toList()));
+        }
+
+
+        return listaPropiedadesAdecuadas;
+    }
+
     public List<Propietario> getListaPropietarios() {
         return listaPropietarios;
+    }
+
+    public List<Reserva> getListaDeReservas() {
+        return listaDeReservas;
     }
 
     public List<Inquilino> getListaInquilinos() {
         return listaInquilinos;
     }
 
-//   Bueno, tenemos que ver ciertas cosas del diseño, yo por hoy lo voy a dejar aca. Pero estaba pensando, crear una interfaz, llamada FiltroDeBusqueda, y hacer que todas las nuevas clases
-    //  La extiendan, asi simplificar los filtrados a un bucle de tipo for.
-
-    public List<Propiedad> buscarPropiedad(String ciudad, LocalDateTime horarioCheckIn, LocalDateTime horarioCheckOut, Integer cantHuespedes, Integer precioMin, Integer precioMax) {
-        List<Propiedad>  listaPropiedadesAdecuadas = new ArrayList<>();
-        for(Propietario propietario : listaPropietarios) {
-            listaPropiedadesAdecuadas.addAll((propietario.getListaPropiedades().stream().filter(propiedad -> propiedad.getCiudad().equals(ciudad))).collect(Collectors.toList()));
-            listaPropiedadesAdecuadas.addAll((propietario.getListaPropiedades().stream().filter(propiedades -> propiedades.getHorarioEntrada() == horarioCheckIn)).collect(Collectors.toList()));
-            listaPropiedadesAdecuadas.addAll((propietario.getListaPropiedades().stream().filter(propiedades -> propiedades.getHorarioSalida() == horarioCheckOut)).collect(Collectors.toList()));
-            listaPropiedadesAdecuadas.addAll((propietario.getListaPropiedades().stream().filter(propiedades -> propiedades.getCapacidad() == cantHuespedes)).collect(Collectors.toList()));
-            listaPropiedadesAdecuadas.addAll((propietario.getListaPropiedades().stream().filter(propiedades -> propiedades.getHorarioEntrada() == horarioCheckIn)).collect(Collectors.toList()));
-            listaPropiedadesAdecuadas.addAll((propietario.getListaPropiedades().stream().filter(propiedades -> propiedades.getPrecio() <= precioMax)).collect(Collectors.toList()));
-            listaPropiedadesAdecuadas.addAll((propietario.getListaPropiedades().stream().filter(propiedades -> propiedades.getPrecio() >= precioMin)).collect(Collectors.toList()));
-        }
-        /* Sospecho que hay algo que va a malir sal en ese filtrado, pero por el momento no se me ocurrio una mejor idea de hacerlo.
+     /* Sospecho que hay algo que va a malir sal en ese filtrado, pero por el momento no se me ocurrio una mejor idea de hacerlo.
         Lo que hace es filtrar de la lista de propiedades de cada propietario y agregar los elementos de la listaPropiedadesAdecuadas.
         Despues lo convierto en un linkedHashSet para eliminar los duplicados, y lo vuelvo castear en un arraylist para respetar los tipos.
          */
-        listaPropiedadesAdecuadas.stream().distinct().collect(Collectors.toList());
+
         /*teoricamente el stream distinct eliminar los elementos duplicados, pero de acuerdo a https://howtodoinjava.com/java/collections/arraylist/remove-duplicate-elements/
         es mejor castearlo a un Hashset
         igual, es algo que iremos viendo.
          */
-        // LinkedHashSet<Integer> listaPropiedadesSinDuplicados = new LinkedHashSet(listaPropiedadesAdecuadas);
-        //  ArrayList<Propiedades> listaSinDuplicados = new ArrayList(listaPropiedadesSinDuplicados);
-        return listaPropiedadesAdecuadas;
-    }
+    // LinkedHashSet<Integer> listaPropiedadesSinDuplicados = new LinkedHashSet(listaPropiedadesAdecuadas);
+    //  ArrayList<Propiedades> listaSinDuplicados = new ArrayList(listaPropiedadesSinDuplicados);
+
 }
 
